d6313c5655f50e784d0979df5c5ebdbc1a0a8787
senemdemirr <senemdemir262@gmail.com>
2022-12-06 18:08:20 +0300

    html edited

--- app/openai-grammar-correction/src/app/grammar-correction/grammer-correction.component.ts
+++ app/openai-grammar-correction/src/app/grammar-correction/grammer-correction.component.ts
@@ -15,7 +15,7 @@ standartEnglishText:any;
 
   constructor(private grammerCorrectionService: GrammerCorrectionService){
 
-    this.text = "Write your english sentence in here"
+    this.text = "Your English"
     this.standartEnglishText = "";
   }
 
