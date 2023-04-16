0a7e226c20c888b8d7f35817ff9766f81dc8d52f
malp <mehmet.alp@rastmobile.com>
2023-04-16 17:31:56 +0300

    added: Upload Audio file from Angular component

--- app/openai-grammar-correction/src/app/grammer-correction.service.ts
+++ app/openai-grammar-correction/src/app/grammer-correction.service.ts
@@ -2,24 +2,28 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GrammerCorrectionService {
+  apiUrl: string = 'http://localhost:4011/';
 
-   apiUrl:string = "http://localhost:4011/";
+  constructor(private http: HttpClient) {}
 
-   constructor(private http: HttpClient) { }
-
-
-  getCorrection(text:string){
-      return this.http.post(`${this.apiUrl}correction`, {
-        "userText": text
-     });
+  getCorrection(text: string) {
+    return this.http.post(`${this.apiUrl}correction`, {
+      userText: text,
+    });
   }
 
-  audioTranscibe(text:string){
+  audioTranscibe(text: string) {
     return this.http.post(`${this.apiUrl}speechToText`, {
-      "userText": text
-   });
-}
+      userText: text,
+    });
+  }
+
+  uploadFile(formData:any){
+    return this.http.post(`${this.apiUrl}audioFile`, {
+      formData
+    });
+  }
 }
--- app/openai-grammar-correction/src/app/speech-to-text/speech-to-text.component.ts
+++ app/openai-grammar-correction/src/app/speech-to-text/speech-to-text.component.ts
@@ -5,38 +5,57 @@ import { GrammerCorrectionService } from '../grammer-correction.service';
 @Component({
   selector: 'app-speech-to-text',
   templateUrl: './speech-to-text.component.html',
-  styleUrls: ['./speech-to-text.component.css']
+  styleUrls: ['./speech-to-text.component.css'],
 })
 export class SpeechToTextComponent {
+  fileName: string = '';
+  text: string;
+  standartEnglishText: any;
 
-
-text:string;
-standartEnglishText:any;
-
-constructor(private grammerCorrectionService: GrammerCorrectionService){
-
-    this.text = ""
-    this.standartEnglishText = "";
+  constructor(private grammerCorrectionService: GrammerCorrectionService) {
+    this.text = '';
+    this.standartEnglishText = '';
   }
 
+  ngOnInit() {}
 
-
-  ngOnInit(){
-
-  }
-
-
-  runAudioFile(){
-    this.grammerCorrectionService.audioTranscibe("").pipe(debounceTime(5000)).subscribe(
-        data => {
-            this.standartEnglishText = data;
-        }, error => {
+  runAudioFile() {
+    this.grammerCorrectionService
+      .audioTranscibe('')
+      .pipe(debounceTime(5000))
+      .subscribe(
+        (data) => {
+          this.standartEnglishText = data;
+        },
+        (error) => {
           alert(error.error.message);
         }
-      )
+      );
   }
 
+  onFileSelected(event:any) {
 
- 
+    const file:File = event.target.files[0];
 
+    if (file) {
+
+        this.fileName = file.name;
+
+        const formData = new FormData();
+
+        formData.append("audioFile", file);
+
+        this.grammerCorrectionService
+        .uploadFile(formData)
+        .pipe(debounceTime(5000))
+        .subscribe(
+          (data) => {
+            this.standartEnglishText = data;
+          },
+          (error) => {
+            alert(error.error.message);
+          }
+        );
+    }
+}
 }
